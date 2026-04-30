85f542b00fb7dbbc23ca6d58dc49fcbff39a7c41
AnuBapatla <anusrichatala@gmail.com>
2026-04-29 23:03:26 -0400

    DropDown list in Blazor

---
+++ Models/Product_Prop.cs
@@ -0,0 +1,11 @@
+﻿namespace BlazorProject1.Models
+{
+    public class Product_Prop
+    {
+        public int Id { get; set; }
+        public string Key { get; set; }
+        public string Value { get; set; }
+        
+
+    }
+}
